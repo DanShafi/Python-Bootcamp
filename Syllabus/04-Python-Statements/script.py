b243c524f6ba6ccb292d326eedfacee2243277fc
Danish Shafi <danish.shafi@live.co.uk>
2020-11-10 12:58:13 +0000

    If, elif and else statements added to revision book

---
+++ Syllabus/04-Python-Statements/script.py
@@ -0,0 +1,29 @@
+# If, Elif and Else Statements
+
+var = 2
+
+if var < 1:
+    print('This is correct')
+else:
+    print('This is incorrect')
+
+
+var1 = 'a'
+
+if var1 == 'b':
+    print('This is a b')
+elif var1 == 'c':
+    print('This is a c')
+else:
+    print('This is a')
+
+name = 'Frankie'
+
+if name == 'Sam':
+    print("Hello Sam")
+elif name == 'Frankie':
+    print('Hello Frankie')
+else:
+    print('We don\'t have you on the system. What is your name?')
+
+# For Loops
---
+++ Syllabus/04-Python-Statements/section-4-notes.md
@@ -0,0 +1,6 @@
+**If, Elif & Else**
+
+-   Mainly concerned in the need for **control flow**.
+-   We use these statements when we only want certain code to execute in a program that execugtes when a particular condition has been met.
+-   Control flow syntax makes use of colons and indentation. This indentation system is crucial to Python and unique to the language.
+-   Any IDE that runs a .py file should automatically indent for you when you run if/else statements.
