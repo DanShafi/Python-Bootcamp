80d473152eb2c9e160340af259ab330b088703b1
Danish Shafi <danish.shafi@live.co.uk>
2020-12-04 15:44:34 +0000

    Coding exercises for module complete

--- Syllabus/05-Python-Methods-and-Functions/script.py
+++ Syllabus/05-Python-Methods-and-Functions/script.py
@@ -39,4 +39,87 @@ mixedup_list = shuffle_list(mylist)
 guess = player_guess()
 check_guess(mixedup_list, guess)
 
-# *args and **kwargs
+# Function Coding Exercises
+
+""" Print Hello World """
+
+
+def myfunc():
+    print('Hello World')
+
+
+""" Define a func that takes in a name arg """
+
+
+def myfunc(name):
+    print('Hello {}'.format(name))
+
+
+""" Define a func that takes in a Bool and return an if statement """
+
+
+def myfunc(x):
+    if x == True:
+        return 'Hello'
+    else:
+        return 'Goodbye'
+
+
+""" Define a func that takes 3 args and return each other in an if logic """
+
+
+def myfunc(x, y, z):
+    if z == True:
+        return x
+    else:
+        return y
+
+
+""" Simple math """
+
+
+def myfunc(x, y):
+    return x + y
+
+
+""" Return a bool if it's even/false """
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def is_greater(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+
+""" Def a func take that *args and returns a list containing only those args """
+
+
+def myfunc(*args):
+    result = []
+
+    for num in args:
+        if num % 2 == 0:
+            result.append(num)
+
+    return result
+
+
+""" Def a func that upper() for each second index in a string """
+
+
+def myfunc(x):
+    out = []
+    for i in range(len(x)):
+        if i % 2 == 0:
+            out.append(x[i].lower())
+        else:
+            out.append(x[i].upper())
+    return ''.join(out)
